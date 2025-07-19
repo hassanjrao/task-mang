4e18e03237261aa37b687f2c34551a1be1fb092b
Hassan <hassanjrao@gmail.com>
2025-07-19 23:36:41 +0400

    updated listing

--- app/Notifications/TaskWebNotification.php
+++ app/Notifications/TaskWebNotification.php
@@ -43,7 +43,7 @@ class TaskWebNotification extends Notification
     {
         return new BroadcastMessage([
             'title' => 'Reminder: ' . $this->task->title,
-            'message' => 'The task ' . $this->task->title .' is due soon.',
+            'message' => 'Task: ' . $this->task->title .' is due soon.',
             'task_id' => $this->task->id,
             'reminder_methods'=> json_decode($this->task->reminder_methods ?? '[]'),
         ]);
